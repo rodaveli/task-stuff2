c190cdfd677bf3e164bada1826dd9bc1c724960a
rodaveli <rodrigoabreu56@gmail.com>
2023-08-01 13:38:22 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-```python
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -41,4 +40,3 @@ class Task(db.Model):
     def complete(self):
         self.completed = True
         db.session.commit()
-```
